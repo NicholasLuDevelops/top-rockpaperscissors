eea4104843527e72e1012c505b390e9b0f6e02b3
NicholasLuDevelops <160670233+NicholasLuDevelops@users.noreply.github.com>
2024-09-07 15:53:02 -0500

    FIXED: getComputerChoice()
    Function now properly returns the correct values.

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 //Generate a number between 1 and 3 (inclusive). Each number is mapped to either Rock, Paper, or Scissors.
 //Side note: If you say Scissor, Paper, Rock instead you are sick in the head kek.
-function getComputerChoice () {
-    let randomChoice = (Math.random * 3) + 1;
-    switch(randomChoice) {
-        case 1:
-            return "Rock";
-        case 2:
-            return "Paper";
-        case 3:
-            return "Scissors";
-    }
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * 3 + 1);
+  switch (randomChoice) {
+    case 1:
+      return "Rock";
+      break;
+    case 2:
+      return "Paper";
+      break;
+    case 3:
+      return "Scissors";
+      break;
+  }
+  return "You fucked the math up bro";
 }
